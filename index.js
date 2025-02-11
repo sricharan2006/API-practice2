0e4f5ecc1db2699c22c170023212da98d052b808
sri charan bukka <charan.bukka@gmail.com>
2025-02-11 15:06:06 +0530

    added and commited the files

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const { resolve } = require('path');
+require('dotenv').config();
 
 const app = express();
-const port = 3010;
+const port = 4000;
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+    if (process.env.IS_ADMIN === 'true') {
+        res.json({ message: "Welcome, Admin!", data: ["Admin Data 1", "Admin Data 2"] });
+    } else {
+        res.json({ message: "Welcome, User!", data: ["User Data 1", "User Data 2"] });
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
